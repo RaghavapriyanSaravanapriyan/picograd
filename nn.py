5f830d3fafb94c7917118bf784b31e0e0ffbd3b1
raghav <s.raghavapriyan@gmail.com>
2026-07-20 22:59:30 +0530

    add : Layers

--- nn.py
+++ nn.py
@@ -25,6 +25,24 @@ class Neuron(Module):
     def __repr__(self):
         return f"{'ReLU ' if self.linearity else 'Linear'}Neuron({len(self.w)})"
 
-n1 = Neuron(5)
-print(n1)
+class Layer(Module):
+
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+    
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+    
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+    
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+l1 = Layer(5,3)
+print(l1)
+
+
+        
+        
 
