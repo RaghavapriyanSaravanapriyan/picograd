68a35c964485ba4d7e51c46cb0cdd332be2fe502
raghav <s.raghavapriyan@gmail.com>
2026-07-20 23:18:17 +0530

    add : MLP implementation using stacked layers

--- nn.py
+++ nn.py
@@ -39,10 +39,21 @@ class Layer(Module):
     
     def __repr__(self):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
-l1 = Layer(5,3)
-print(l1)
+    
+class MLP(Module):
 
+    def __init__(self, nin, nout):
+        sz = [nin] + nout
+        self.layers = [Layer(sz[i], sz[i+1], nlin=i!=len(nout)-1) for i in range(len(nout))]
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
 
-        
-        
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
 
